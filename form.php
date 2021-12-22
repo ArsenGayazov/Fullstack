2ac521d3e544ae39409ed6e444731e18b1d46a72
ArsenGayazov <arsengayazov89@gmail.com>
2021-12-22 20:30:32 +0300

    форма создания пользователей

--- form.php
+++ form.php
@@ -1,6 +1,20 @@
 <?php
 
-$name = $_POST['name'];
-$age = $_POST['age'];
+$user = 'root'; $password = '';
+$pdo = new Pdo('mysql:dbname=fullstack;host=127.0.0.1', $user, $password);
 
-echo "Hello, $name, you are $age years old!";
+$name = $_POST['name'];
+$login = $_POST['login'];
+$password = $_POST['password'];
+$city_id = $_POST['city_id'];
+
+$query = "INSERT INTO users (name, login, password, city_id) VALUES (:name, :login, :password, :city_id)";
+$res = $pdo->prepare($query);
+$res->execute([
+    ':name' => $name,
+    ':login' => $login,
+    ':password' => $password,
+    ':city_id' => $city_id,
+]);
+
+header('Location: index.php'); //возвращение на страницу индекс.пхп
