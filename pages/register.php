770aa1bfc09fec9a195fdfc22765ca08b8e6d580
ArsenGayazov <arsengayazov89@gmail.com>
2022-01-09 20:41:37 +0300

    регистрация, админка

--- pages/register.php
+++ pages/register.php
@@ -1,12 +1,26 @@
 <?php
-$title = 'Рагистрация';
+$title = 'Регистрация';
 require_once '../templates/header.php';
-?>
 
-<form method="POST" action="form.php">
+$query = "SELECT * FROM cities";
+$res = $pdo->query($query);
+$cities = $res->fetchAll();
+
+if (isset($_SESSION['registerError'])) {
+    echo 
+    "
+    <div class='alert alert-warning text-center' role='alert'>
+        {$_SESSION['registerError']}
+    </div>
+    ";
+    unset($_SESSION['registerError']);
+}
+?>
+<form method="POST" action="../actions/register.php">
   <input requred class="form-control mb-2" placeholder="Имя" name='name'>
   <input class="form-control mb-2" placeholder="Логин" name='login'>
   <input class="form-control mb-2" type="password" placeholder="Пароль" name='password'>
+  <input class="form-control mb-2" type="password" placeholder="Повторите пароль" name='repassword'>
   <select class="form-control mb-2" name="city_id">
       <option selected disabled>-- Выберете город --</option>
       <?php
